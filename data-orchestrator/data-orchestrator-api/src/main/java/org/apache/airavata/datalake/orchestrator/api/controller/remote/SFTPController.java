ace1dc1bdfd9b3058574686cb1fc719901700bd8
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2021-01-05 19:00:19 -0500

    Adding repository support for SFTP Remotes

--- data-orchestrator/data-orchestrator-api/src/main/java/org/apache/airavata/datalake/orchestrator/api/controller/remote/SFTPController.java
+++ data-orchestrator/data-orchestrator-api/src/main/java/org/apache/airavata/datalake/orchestrator/api/controller/remote/SFTPController.java
@@ -19,39 +19,61 @@
  */
 package org.apache.airavata.datalake.orchestrator.api.controller.remote;
 
-import org.apache.airavata.datalake.orchestrator.api.model.remote.SFTPCredential;
+import org.apache.airavata.datalake.orchestrator.api.db.entity.SFTPRemoteEntity;
+import org.apache.airavata.datalake.orchestrator.api.db.repo.SFTPRemoteRepository;
 import org.apache.airavata.datalake.orchestrator.api.model.remote.SFTPRemote;
+import org.dozer.DozerBeanMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/remotes/sftp")
 public class SFTPController {
 
+    @Autowired
+    private SFTPRemoteRepository sftpRemoteRepository;
+
     @GetMapping(value = "/{remoteId}", produces = "application/json")
     public SFTPRemote fetchSFTPRemote(@PathVariable(name = "remoteId") String remoteId) {
-        return new SFTPRemote()
-            .setHost("localhost")
-            .setPort(22)
-            .setCredential(
-                    new SFTPCredential()
-                        .setAuthMethod(SFTPCredential.AuthMethod.SSH_KEY)
-                        .setPrivateKey("")
-                        .setPublicKey(""));
+
+        Optional<SFTPRemoteEntity> entityOp = sftpRemoteRepository.findById(remoteId);
+        SFTPRemoteEntity sftpRemoteEntity = entityOp.orElseThrow(
+                                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, remoteId + " not found"));
+        DozerBeanMapper mapper = new DozerBeanMapper();
+        return mapper.map(sftpRemoteEntity, SFTPRemote.class);
     }
 
     @PostMapping(value = "")
     public String createSFTPRemote(@RequestBody SFTPRemote sftpRemote) {
-        return "Remote id";
+
+        DozerBeanMapper mapper = new DozerBeanMapper();
+        SFTPRemoteEntity sftpRemoteEntity = mapper.map(sftpRemote, SFTPRemoteEntity.class);
+        SFTPRemoteEntity saved = sftpRemoteRepository.save(sftpRemoteEntity);
+        return saved.getId();
     }
 
     @PutMapping(value = "/{remoteId}")
     public String updateSFTPRemote(@RequestBody SFTPRemote sftpRemote,
                                    @PathVariable(name = "remoteId") String remoteId) {
-        return "Remote id";
+
+        DozerBeanMapper mapper = new DozerBeanMapper();
+        SFTPRemoteEntity sftpRemoteEntity = mapper.map(sftpRemote, SFTPRemoteEntity.class);
+        sftpRemoteEntity.setId(remoteId);
+        SFTPRemoteEntity saved = sftpRemoteRepository.save(sftpRemoteEntity);
+        return saved.getId();
     }
 
     @DeleteMapping(value = "/{remoteId}")
     public String removeSFTPRemote(@PathVariable(name = "remoteId") String remoteId) {
-        return "Deleted";
+
+        if (! sftpRemoteRepository.existsById(remoteId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, remoteId + " not found");
+        }
+        sftpRemoteRepository.deleteById(remoteId);
+        return "Success";
     }
 }
