e76ee502d6c4f85629b856a2f33067c83b92b397
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2021-09-07 04:27:04 -0400

    Generalizing ansible vars, adding zookeepr role, kafka installation, migrating to new backup instance

--- data-orchestrator/data-orchestrator-service/data-orchestrator-api-server/src/main/java/org/apache/airavata/datalake/orchestrator/OrchClient.java
+++ data-orchestrator/data-orchestrator-service/data-orchestrator-api-server/src/main/java/org/apache/airavata/datalake/orchestrator/OrchClient.java
@@ -25,12 +25,14 @@ public class OrchClient {
 
         mapper.map(dpe, builder);
         System.out.println(builder.build());*/
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext().build();
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6566).usePlaintext().build();
         DataParserServiceGrpc.DataParserServiceBlockingStub parserClient = DataParserServiceGrpc.newBlockingStub(channel);
 
-        ParsingJobListResponse parsingJobListResponse = parserClient.listParsingJobs(ParsingJobListRequest.newBuilder().build());
-        System.out.println(parsingJobListResponse);
-        /*String parserId = UUID.randomUUID().toString();
+        //ParsingJobListResponse parsingJobListResponse = parserClient.listParsingJobs(ParsingJobListRequest.newBuilder().build());
+        //System.out.println(parsingJobListResponse);
+        String parserId = UUID.randomUUID().toString();
+        String dm3InputId = UUID.randomUUID().toString();
+        String jsonOutputId = UUID.randomUUID().toString();
         ParserRegisterResponse parserRegisterResponse = parserClient.registerParser(ParserRegisterRequest.newBuilder()
                 .setParser(DataParser.newBuilder()
                         .setParserId(parserId)
@@ -41,12 +43,12 @@ public class OrchClient {
                         .setOutputPath("/opt/outputs")
                         .addInputInterfaces(DataParserInputInterface.newBuilder()
                                 .setParserId(parserId)
-                                .setParserInputInterfaceId(UUID.randomUUID().toString())
+                                .setParserInputInterfaceId(dm3InputId)
                                 .setInputType("FILE")
                                 .setInputName("input.dm3").build())
                         .addOutputInterfaces(DataParserOutputInterface.newBuilder()
                                 .setParserId(parserId)
-                                .setParserOutputInterfaceId(UUID.randomUUID().toString())
+                                .setParserOutputInterfaceId(jsonOutputId)
                                 .setOutputType("FILE")
                                 .setOutputName("derrived-metadata.json")
                                 .build())
@@ -66,19 +68,20 @@ public class OrchClient {
         String parsingJobId = UUID.randomUUID().toString();
         ParsingJobRegisterRequest.Builder parsingJob = ParsingJobRegisterRequest.newBuilder().setParsingJob(DataParsingJob.newBuilder()
                 .setDataParsingJobId(parsingJobId)
-                .setParserId("2f48d838-d0e4-4413-8d8e-4579d4a7369d")
+                .setParserId(parserId)
                 .addDataParsingJobInputs(DataParsingJobInput.newBuilder()
                         .setId(UUID.randomUUID().toString())
-                        .setDataParserInputInterfaceId("dbb221fc-96d1-4aaf-ae85-73d6215a5106")
+                        .setDataParserInputInterfaceId(dm3InputId)
                         .setDataParsingJobId(parsingJobId)
                         .setSelectionQuery("EXTENSION == dm3").build())
                 .addDataParsingJobOutputs(DataParsingJobOutput.newBuilder()
                         .setId(UUID.randomUUID().toString())
                         .setDataParsingJobId(parsingJobId)
                         .setOutputType("JSON")
-                        .setDataParserOutputInterfaceId("d1ded2cc-ffe6-4e47-a5a2-bdb782b62359").build()).build());
-        ParsingJobRegisterResponse parsingJobRegisterResponse = parserClient.registerParsingJob(parsingJob.build()); */
+                        .setDataParserOutputInterfaceId(jsonOutputId).build()).build());
+        ParsingJobRegisterResponse parsingJobRegisterResponse = parserClient.registerParsingJob(parsingJob.build());
 
+        System.out.println(parsingJobRegisterResponse);
         /*File f = new File("/tmp/a.dm3");
 
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
@@ -86,6 +89,6 @@ public class OrchClient {
         bindings.put("polyglot.js.allowHostAccess", true);
         bindings.put("polyglot.js.allowHostClassLookup", (Predicate<String>) s -> true);
         bindings.put("inputFile", f);
-        System.out.println(engine.eval("inputFile.getName().endsWith(\".dm3\");")); // it will not work without allowHostAccess and allowHostClassLookup*/
+        System.out.println(engine.eval("metadata.getFriendlyName().endsWith(\".dm3\");")); // it will not work without allowHostAccess and allowHostClassLookup*/
     }
  }
