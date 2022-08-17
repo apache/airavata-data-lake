1212dbf9d7c365b2cb009d514246f909d58aac4f
Isuru Ranawaka <irjanith@gmail.com>
2022-08-17 17:02:18 -0400

    fix search storage preference type

--- data-resource-management-service/drms-rdbms-impl/drms-server/src/main/java/org/apache/airavata/drms/api/persistance/mapper/StoragePreferenceMapper.java
+++ data-resource-management-service/drms-rdbms-impl/drms-server/src/main/java/org/apache/airavata/drms/api/persistance/mapper/StoragePreferenceMapper.java
@@ -3,6 +3,7 @@ package org.apache.airavata.drms.api.persistance.mapper;
 import org.apache.airavata.datalake.drms.storage.AnyStorage;
 import org.apache.airavata.datalake.drms.storage.AnyStoragePreference;
 import org.apache.airavata.datalake.drms.storage.preference.s3.S3StoragePreference;
+import org.apache.airavata.datalake.drms.storage.preference.sda.SDAStoragePreference;
 import org.apache.airavata.datalake.drms.storage.preference.ssh.SSHStoragePreference;
 import org.apache.airavata.drms.api.persistance.model.Resource;
 import org.apache.airavata.drms.api.persistance.model.ResourceProperty;
@@ -44,6 +45,12 @@ public class StoragePreferenceMapper {
                 S3StoragePreference.Builder s3Builder = S3StoragePreference.newBuilder();
                 s3Builder.setStorage(anyStorage.getS3Storage());
                 anyStoragePrefBuilder.setS3StoragePreference(s3Builder.build());
+                setObjectFieldsUsingMap(anyStoragePrefBuilder, resource);
+                break;
+            case StoragePreferenceConstants.SDA_STORAGE_PREFERENCE_TYPE_LABEL:
+                SDAStoragePreference.Builder sdaBuilder = SDAStoragePreference.newBuilder();
+                sdaBuilder.setStorage(anyStorage.getSshStorage());
+                anyStoragePrefBuilder.setSdaStoragePreference(sdaBuilder.build());
                 setObjectFieldsUsingMap(anyStoragePrefBuilder, resource);
                 break;
             default:
